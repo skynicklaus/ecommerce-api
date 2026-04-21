112d61aded60766ebd60b84b0343ddc44742e74b
skynicklaus <sky.nicklaus@gmail.com>
2026-04-22 00:07:50 +0800

    added role tests

--- db/sqlc/role_test.go
+++ db/sqlc/role_test.go
@@ -2,10 +2,9 @@ package db_test
 
 import (
 	"context"
-	"crypto/rand"
 	"fmt"
-	"math/big"
 	"testing"
+	"time"
 
 	"github.com/go-openapi/testify/v2/require"
 	"github.com/google/uuid"
@@ -14,20 +13,13 @@ import (
 	"github.com/skynicklaus/ecommerce-api/util"
 )
 
-func createRandomRole(t *testing.T) db.Role {
-	n, err := rand.Int(rand.Reader, big.NewInt(2))
-	require.NoError(t, err)
-	require.NotEmpty(t, n)
-
+func createRandomRoleWithOrg(t *testing.T, organization *db.Organization) db.Role {
 	organizationType := util.GetRandomOrganizationType(t)
-	require.NotEmpty(t, organizationType)
-
 	isSystem := true
 	slug := util.GetRandomString(t, 8)
 	var organizationID *uuid.UUID
-	if n.Int64() == 0 {
+	if organization != nil {
 		isSystem = false
-		organization := createRandomOrganization(t)
 		organizationID = &organization.ID
 		organizationType = organization.Type
 		slug = fmt.Sprintf("%s.%s", organization.Slug, slug)
@@ -46,14 +38,63 @@ func createRandomRole(t *testing.T) db.Role {
 	require.NotEmpty(t, role)
 
 	require.Equal(t, arg.RoleName, role.RoleName)
+	require.Equal(t, arg.OrganizationID, role.OrganizationID)
 	require.Equal(t, arg.OrganizationType, role.OrganizationType)
 	require.Equal(t, arg.Slug, role.Slug)
 	require.Equal(t, arg.IsSystem, role.IsSystem)
 	require.NotZero(t, role.CreatedAt)
-
-	if !isSystem {
-		require.Equal(t, arg.OrganizationID, role.OrganizationID)
-	}
+	require.NotZero(t, role.UpdatedAt)
 
 	return role
 }
+
+func createRandomRole(t *testing.T) db.Role {
+	n := util.CoinFlip(t)
+
+	if n == 1 {
+		org := createRandomOrganization(t)
+		return createRandomRoleWithOrg(t, &org)
+	}
+
+	return createRandomRoleWithOrg(t, nil)
+}
+
+func TestCreateRole(t *testing.T) {
+	createRandomRole(t)
+}
+
+func TestGetRoleByID(t *testing.T) {
+	role1 := createRandomRole(t)
+
+	role2, err := testStore.GetRoleByID(context.Background(), role1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, role2)
+
+	require.Equal(t, role1.ID, role2.ID)
+	require.Equal(t, role1.OrganizationID, role2.OrganizationID)
+	require.Equal(t, role1.OrganizationType, role2.OrganizationType)
+	require.Equal(t, role1.RoleName, role2.RoleName)
+	require.Equal(t, role1.Slug, role2.Slug)
+	require.Equal(t, role1.IsSystem, role2.IsSystem)
+	require.WithinDuration(t, role1.CreatedAt, role2.CreatedAt, time.Second)
+	require.WithinDuration(t, role1.UpdatedAt, role2.UpdatedAt, time.Second)
+}
+
+func TestGetRoleBySlug(t *testing.T) {
+	role1 := createRandomRole(t)
+
+	role2, err := testStore.GetRoleBySlug(context.Background(), role1.Slug)
+	require.NoError(t, err)
+	require.NotEmpty(t, role2)
+
+	require.Equal(t, role1.ID, role2.ID)
+	require.Equal(t, role1.OrganizationID, role2.OrganizationID)
+	require.Equal(t, role1.OrganizationType, role2.OrganizationType)
+	require.Equal(t, role1.RoleName, role2.RoleName)
+	require.Equal(t, role1.Slug, role2.Slug)
+	require.Equal(t, role1.IsSystem, role2.IsSystem)
+	require.WithinDuration(t, role1.CreatedAt, role2.CreatedAt, time.Second)
+	require.WithinDuration(t, role1.UpdatedAt, role2.UpdatedAt, time.Second)
+}
+
+// TODO: Test ListOrganizationRolesByType
