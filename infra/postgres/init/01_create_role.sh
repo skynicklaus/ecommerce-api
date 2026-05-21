59fe37510eda71b7203085b26105ed9c4be0b3db
skynicklaus <sky.nicklaus@gmail.com>
2026-05-21 21:55:16 +0800

    grant privileges to app_system

--- infra/postgres/init/01_create_role.sh
+++ infra/postgres/init/01_create_role.sh
@@ -5,4 +5,6 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE ROLE app_system LOGIN PASSWORD '$APP_SYSTEM_PASSWORD' BYPASSRLS;
     CREATE ROLE app_user LOGIN PASSWORD '$APP_USER_PASSWORD';
+    GRANT ALL ON SCHEMA public TO app_system;
+    GRANT app_user TO app_system WITH ADMIN OPTION;
 EOSQL
