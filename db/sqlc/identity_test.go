ea05569b4587f255e959da3075d651271ffe79d2
skynicklaus <sky.nicklaus@gmail.com>
2026-05-23 02:35:00 +0800

    complete identity test

--- db/sqlc/identity_test.go
+++ db/sqlc/identity_test.go
@@ -1,20 +1,23 @@
+//go:build integration
+
 package db_test
 
 import (
-	"context"
 	"testing"
+	"time"
 
-	"github.com/go-openapi/testify/v2/require"
+	"github.com/stretchr/testify/require"
 
 	db "github.com/skynicklaus/ecommerce-api/db/sqlc"
 	"github.com/skynicklaus/ecommerce-api/util"
 )
 
 func createRandomIdentity(t *testing.T) db.Identity {
+	t.Helper()
 	identityType := util.GetRandomIdentityType(t)
 	require.NotEmpty(t, identityType)
 
-	identity, err := testStore.CreateIdentity(context.Background(), identityType)
+	identity, err := testStore.CreateIdentity(t.Context(), identityType)
 	require.NoError(t, err)
 	require.NotEmpty(t, identity)
 
@@ -26,4 +29,16 @@ func createRandomIdentity(t *testing.T) db.Identity {
 
 func TestCreateIdentity(t *testing.T) {
 	createRandomIdentity(t)
+}
+
+func TestGetIdentity(t *testing.T) {
+	identity1 := createRandomIdentity(t)
+
+	identity2, err := testStore.GetIdentity(t.Context(), identity1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, identity2)
+
+	require.Equal(t, identity1.ID, identity2.ID)
+	require.Equal(t, identity1.Type, identity2.Type)
+	require.WithinDuration(t, identity1.CreatedAt, identity2.CreatedAt, 5*time.Second)
 }
