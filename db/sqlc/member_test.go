366e8fdef0b1aea8e85d7a5bd13e2cdd844188ec
skynicklaus <sky.nicklaus@gmail.com>
2026-05-23 02:35:19 +0800

    complete member test

--- db/sqlc/member_test.go
+++ db/sqlc/member_test.go
@@ -1,15 +1,18 @@
+//go:build integration
+
 package db_test
 
 import (
-	"context"
 	"testing"
 
-	"github.com/go-openapi/testify/v2/require"
+	"github.com/stretchr/testify/require"
 
 	db "github.com/skynicklaus/ecommerce-api/db/sqlc"
+	"github.com/skynicklaus/ecommerce-api/util"
 )
 
 func createRandomMember(t *testing.T) db.Member {
+	t.Helper()
 	identity := createRandomIdentity(t)
 	organization := createRandomOrganization(t)
 
@@ -18,7 +21,7 @@ func createRandomMember(t *testing.T) db.Member {
 		OrganizationID: organization.ID,
 	}
 
-	member, err := testStore.CreateMember(context.Background(), arg)
+	member, err := testStore.CreateMember(t.Context(), arg)
 	require.NoError(t, err)
 	require.NotEmpty(t, member)
 
@@ -30,4 +33,42 @@ func createRandomMember(t *testing.T) db.Member {
 
 func TestCreateMember(t *testing.T) {
 	createRandomMember(t)
+}
+
+func TestGetMemberByIdentityID(t *testing.T) {
+	member1 := createRandomMember(t)
+
+	member2, err := testStore.GetMemberByIdentityID(t.Context(), member1.IdentityID)
+	require.NoError(t, err)
+	require.NotEmpty(t, member2)
+
+	require.Equal(t, member1.ID, member2.ID)
+	require.Equal(t, member1.IdentityID, member2.IdentityID)
+	require.Equal(t, member1.OrganizationID, member2.OrganizationID)
+}
+
+func TestCountPlatformAdmins(t *testing.T) {
+	initialCount, err := testStore.CountPlatformAdmins(t.Context())
+	require.NoError(t, err)
+
+	orgArg := db.CreateOrganizationParams{
+		Name:     "Platform Org " + util.GetRandomString(t, 6),
+		Slug:     "platform-org-" + util.GetRandomString(t, 6),
+		Type:     string(util.OrganizationTypePlatform),
+		Status:   string(util.OrganizationStatusActive),
+		Metadata: []byte(`{}`),
+	}
+	org, err := testStore.CreateOrganization(t.Context(), orgArg)
+	require.NoError(t, err)
+
+	identity := createRandomIdentity(t)
+	_, err = testStore.CreateMember(t.Context(), db.CreateMemberParams{
+		IdentityID:     identity.ID,
+		OrganizationID: org.ID,
+	})
+	require.NoError(t, err)
+
+	newCount, err := testStore.CountPlatformAdmins(t.Context())
+	require.NoError(t, err)
+	require.Equal(t, initialCount+1, newCount)
 }
