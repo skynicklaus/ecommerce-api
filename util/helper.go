908b5b5e7774f7c5347fe3e6a6f16b990dd6a586
skynicklaus <sky.nicklaus@gmail.com>
2026-05-24 21:17:31 +0800

    added token hash helper

--- util/helper.go
+++ util/helper.go
@@ -1,6 +1,17 @@
 package util
 
-import "time"
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"time"
+)
+
+// HashSessionToken returns the lowercase hex-encoded SHA-256 of rawToken.
+// Store the hash in the DB; give rawToken to the client once.
+func HashSessionToken(rawToken string) string {
+	h := sha256.Sum256([]byte(rawToken))
+	return hex.EncodeToString(h[:])
+}
 
 func DeferString(s *string) string {
 	if s == nil {
