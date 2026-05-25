ef4a635e411720102396668b0780891a568838be
skynicklaus <sky.nicklaus@gmail.com>
2026-05-26 05:02:50 +0800

    added cache di

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -4,16 +4,19 @@ import (
 	"golang.org/x/sync/singleflight"
 
 	db "github.com/skynicklaus/ecommerce-api/db/sqlc"
+	"github.com/skynicklaus/ecommerce-api/internal/cache"
 )
 
 type Middleware struct {
 	store    db.Store
+	cache    *cache.RedisClient
 	renewalG singleflight.Group
 }
 
-func New(store db.Store) *Middleware {
+func New(store db.Store, cache *cache.RedisClient) *Middleware {
 	return &Middleware{
 		store:    store,
+		cache:    cache,
 		renewalG: singleflight.Group{},
 	}
 }
