0fd1063b1466e16c3ce22fa7d369936322c0b9b0
skynicklaus <sky.nicklaus@gmail.com>
2026-05-26 04:57:40 +0800

    direct database value assert

--- db/sqlc/member_role_test.go
+++ db/sqlc/member_role_test.go
@@ -22,20 +22,40 @@ func randomAssignRoleToMember(t *testing.T) {
 	require.NoError(t, err)
 	require.NotEmpty(t, n)
 
-	var uuid *uuid.UUID
+	var assignedBy *uuid.UUID
 	if n.Int64() == 1 {
 		oldMember := createRandomMember(t)
-		uuid = &oldMember.ID
+		assignedBy = &oldMember.ID
 	}
 
 	arg := db.AssignRoleToMemberParams{
 		MemberID:   member.ID,
 		RoleID:     role.ID,
-		AssignedBy: uuid,
+		AssignedBy: assignedBy,
 	}
 
 	err = testStore.AssignRoleToMember(t.Context(), arg)
 	require.NoError(t, err)
+
+	// Direct database read-back verification
+	var dbMemberID uuid.UUID
+	var dbRoleID int16
+	var dbAssignedBy *uuid.UUID
+	err = testPool.QueryRow(
+		t.Context(),
+		"SELECT member_id, role_id, assigned_by FROM member_roles WHERE member_id = $1 AND role_id = $2",
+		member.ID,
+		role.ID,
+	).Scan(&dbMemberID, &dbRoleID, &dbAssignedBy)
+	require.NoError(t, err)
+	require.Equal(t, member.ID, dbMemberID)
+	require.Equal(t, role.ID, dbRoleID)
+	if assignedBy != nil {
+		require.NotNil(t, dbAssignedBy)
+		require.Equal(t, *assignedBy, *dbAssignedBy)
+	} else {
+		require.Nil(t, dbAssignedBy)
+	}
 }
 
 func TestAssignRoleToMember(t *testing.T) {
